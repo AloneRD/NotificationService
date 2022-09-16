7cf8df15bf2faeb57f74fdad15a3f41fea5a24b3
Artur <artur_goncharov_97@mail.ru>
2022-09-16 22:15:42 +0300

    created model Client

--- notification_service/notification/models.py
+++ notification_service/notification/models.py
@@ -1,4 +1,6 @@
+import pytz
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class MailingList(models.Model):
@@ -13,3 +15,27 @@ class MailingList(models.Model):
 
     def __str__(self):
         return f'Рассылка {self.id}'
+
+
+class Client(models.Model):
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+
+    phonenumber = PhoneNumberField(verbose_name='номер телефона')
+    tag = models.CharField(
+        max_length=100,
+        db_index=True,
+        blank=True,
+        verbose_name='тег'
+        )
+    timezone = models.CharField(
+        max_length=35,
+        choices=TIMEZONES,
+        verbose_name='часовой пояс'
+        )
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+
+    def __str__(self):
+        return f'Клиент {self.id}'
