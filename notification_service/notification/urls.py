e0f5ac66cf99cf987f02827712977509dfe70647
Artur <artur_goncharov_97@mail.ru>
2022-09-16 23:25:19 +0300

    added router

--- notification_service/notification/urls.py
+++ notification_service/notification/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from notification import views
+
+
+router = DefaultRouter()
+router.register(r'mailing-list', views.MailingListViewSet)
+router.register(r'client', views.ClientViewSet)
+router.register(r'message', views.MessageViewSet)
+print(router.urls)
 
 urlpatterns = [
-    
+    path('v1/', include(router.urls))
 ]
