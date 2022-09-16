4c1dcfad644140caf4a9bc12510a481d610f767f
Artur <artur_goncharov_97@mail.ru>
2022-09-16 22:44:30 +0300

    added model Client

--- notification_service/notification/models.py
+++ notification_service/notification/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class MailingList(models.Model):
     date_time_start = models.DateTimeField(verbose_name='дата и время')
-    message = models.TextField(verbose_name='сообщение')
+    message_text = models.TextField(verbose_name='текст сообщение')
     tag = models.CharField(max_length=100, db_index=True, verbose_name='тег')
     date_time_finish = models.DateTimeField(verbose_name='дата и время')
 
@@ -39,3 +39,36 @@ class Client(models.Model):
 
     def __str__(self):
         return f'Клиент {self.id}'
+
+
+class Message(models.Model):
+    STATUS = [
+        ('SENT', 'Sent'),
+        ('NO SENT', 'No sent')
+    ]
+    date_time_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата и время отправки сообщения'
+        )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS,
+        verbose_name='статус отправки'
+        )
+    mailing = models.ForeignKey(
+        MailingList,
+        on_delete=models.CASCADE,
+        related_name='messages'
+        )
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        related_name='messages'
+        )
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
+
+    def __str__(self):
+        return f'Сообщение {self.id} клиенту {self.client} '
