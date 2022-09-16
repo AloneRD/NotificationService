1a23f7600ec608f56c2d7c35268a7a2120df968e
Artur <artur_goncharov_97@mail.ru>
2022-09-16 22:50:38 +0300

    register mpdels in admin site

--- notification_service/notification/admin.py
+++ notification_service/notification/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MailingList, Client, Message
 
-# Register your models here.
+admin.site.register(MailingList)
+admin.site.register(Client)
+admin.site.register(Message)
