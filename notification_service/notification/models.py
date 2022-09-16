346f816e41a01d67736fcd3f56fad061b0d91dc0
Artur <artur_goncharov_97@mail.ru>
2022-09-16 21:48:49 +0300

    create model MailingList

--- notification_service/notification/models.py
+++ notification_service/notification/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MailingList(models.Model):
+    date_time_start = models.DateTimeField(verbose_name='дата и время')
+    message = models.TextField(verbose_name='сообщение')
+    tag = models.CharField(max_length=100, db_index=True, verbose_name='тег')
+    date_time_finish = models.DateTimeField(verbose_name='дата и время')
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+    def __str__(self):
+        return f'Рассылка {self.id}'
