839cb543005dffe574d9ec01e1cf1a8584f15d46
Artur <artur_goncharov_97@mail.ru>
2022-09-16 23:02:10 +0300

    create MailingListViewSet, ClientViewSet, MessageViewSet

--- notification_service/notification/views.py
+++ notification_service/notification/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from .models import MailingList, Client, Message
+from rest_framework import viewsets
 
-# Create your views here.
+
+class MailingListViewSet(viewsets.ModelViewSet):
+    model = MailingList
+    queryset = MailingList.objects.all()
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    model = Client
+    queryset = Client.objects.all()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    model = Message
+    queryset = Message.objects.all()
