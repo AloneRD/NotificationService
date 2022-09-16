5d10c4f7ab001363317d71a6a96308abe87b0bce
Artur <artur_goncharov_97@mail.ru>
2022-09-16 23:24:19 +0300

    fix ViewSets

--- notification_service/notification/views.py
+++ notification_service/notification/views.py
@@ -1,17 +1,18 @@
 from .models import MailingList, Client, Message
+from .serializers import MailingListSerializer, ClientSerializer, MessageSerializer
 from rest_framework import viewsets
 
 
 class MailingListViewSet(viewsets.ModelViewSet):
-    model = MailingList
+    serializer_class = MailingListSerializer
     queryset = MailingList.objects.all()
 
 
 class ClientViewSet(viewsets.ModelViewSet):
-    model = Client
+    serializer_class = ClientSerializer
     queryset = Client.objects.all()
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    model = Message
+    serializer_class = MessageSerializer
     queryset = Message.objects.all()
