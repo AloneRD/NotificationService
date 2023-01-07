64c7a0b81ecd81c235c7257cdc54e7ed2b4cbcf8
Artur <artur_goncharov_97@mail.ru>
2023-01-07 21:03:01 +0300

    added get_total_stats

--- notification_service/notification/views.py
+++ notification_service/notification/views.py
@@ -1,11 +1,26 @@
+import json
 from .models import MailingList, Client, Message
 from .serializers import MailingListSerializer, ClientSerializer, MessageSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
 
 
 class MailingListViewSet(viewsets.ModelViewSet):
     serializer_class = MailingListSerializer
     queryset = MailingList.objects.all()
+
+    @action(detail=False)
+    def get_total_stats(self, request):
+        total_mailing_list = MailingList.objects.count()
+        messages_no_sent, messages_sent = Message.objects.all().values('status').annotate(total=Count('status'))
+        content = {
+            "Общее колличество рассылок": total_mailing_list,
+            "Колличество отправленных сообщений": messages_sent['total'],
+            "Колличество не отправленных сообщений": messages_no_sent['total']
+            }
+        return Response(content)
 
 
 class ClientViewSet(viewsets.ModelViewSet):
